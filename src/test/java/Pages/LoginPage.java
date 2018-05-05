8f3a672b016cdf4f42f32017e347f09180244ec4
riggs84 <victor.iurkov@gmail.com>
2018-05-05 16:46:18 -0400

    login page class added

--- src/test/java/Pages/LoginPage.java
+++ src/test/java/Pages/LoginPage.java
@@ -2,6 +2,7 @@ package Pages;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
@@ -18,5 +19,10 @@ public class LoginPage {
 
     public void openPage(){
         driver.get("http://blazedemo.com/login");
+    }
+
+    public void moveMouseToLoginBtn(){
+        Actions builder = new Actions(driver);
+        builder.moveToElement(loginBtn);
     }
 }
